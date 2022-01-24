72e3baf5b401992a9b16b48e7a7fdf8c5ca02481
ShlapinDmitry <85103953+ShlapinDmitry@users.noreply.github.com>
2022-01-24 12:21:32 +0300

    Create .parameters.php

---
+++ hometask.auto/install/components/hometask/catalog.detail/.parameters.php
@@ -0,0 +1,35 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
+
+if (!CModule::IncludeModule('iblock')) {
+    return;
+}
+
+/*
+ * Настройки компонента
+ */
+$arComponentParameters = array(
+    "GROUPS" => array(),
+    'PARAMETERS' => array(
+
+        // идентификатор элемента получать из $_REQUEST["ELEMENT_ID"]
+        'ELEMENT_ID' => array(
+            'PARENT' => 'BASE',
+            'NAME' => 'Идентификатор элемента',
+            'TYPE' => 'STRING',
+            'DEFAULT' => '={$_REQUEST["ELEMENT_ID"]}',
+        ),
+
+        // шаблон ссылки на страницу элемента
+        'ELEMENT_URL' => array(
+            'PARENT' => 'URL_TEMPLATES',
+            'NAME' => 'URL, ведущий на страницу с содержимым элемента',
+            'TYPE' => 'STRING',
+            'DEFAULT' => 'detail/#ELEMENT_ID#/'
+        ),
+    ),
+);
+
+// добавляем еще одну настройку — на случай, если элемент инфоблока не найден
+CIBlockParameters::Add404Settings($arComponentParameters, $arCurrentValues);
